3fd5551aac1751d1de446762119689971661ea5b
Gajjar Mitul <gajjarmitul007@gmail.com>
2021-03-18 20:24:37 +0530

    20210318 -1024

--- resources/views/front/layout/header.blade.php
+++ resources/views/front/layout/header.blade.php
@@ -28,5 +28,3 @@
         </div>
     </div>
 </header>
-
-@include('front.layout.hero')
--- resources/views/front/logo_design.blade.php
+++ resources/views/front/logo_design.blade.php
@@ -10,14 +10,45 @@
 @section('styles')
 @endsection
 
+
+@section('hero')
+    <section class="hero-section inner-page">
+      <div class="wave">
+
+        <svg width="1920px" height="265px" viewBox="0 0 1920 265" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+          <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+            <g id="Apple-TV" transform="translate(0.000000, -402.000000)" fill="#FFFFFF">
+              <path d="M0,439.134243 C175.04074,464.89273 327.944386,477.771974 458.710937,477.771974 C654.860765,477.771974 870.645295,442.632362 1205.9828,410.192501 C1429.54114,388.565926 1667.54687,411.092417 1920,477.771974 L1920,667 L1017.15166,667 L0,667 L0,439.134243 Z" id="Path"></path>
+            </g>
+          </g>
+        </svg>
+
+      </div>
+
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-12">
+            <div class="row justify-content-center">
+              <div class="col-md-7 text-center hero-text">
+                <h1 data-aos="fade-up" data-aos-delay="">Logo Designing</h1>
+                <!-- <p class="mb-5" data-aos="fade-up" data-aos-delay="100">Create Your App With Us</p> -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </section>
+@endsection
+
+
 @section('content')
     <section class="section pb-0">
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-md-4 mr-auto">
                     <h2 class="mb-4">Seamlessly Communicate</h2>
-                    <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur at reprehenderit optio, laudantium eius quod, eum maxime molestiae porro omnis. Dolores aspernatur delectus impedit incidunt dolore mollitia esse natus beatae.</p>
-                    <p><a href="#">Read More</a></p>
+                    <p class="mb-4">Creative graphic designs help to construct an uncommon picture of an organization. Be it making a logo, planning and organization handout, making a creative pennant, or an imaginative video.</p>
                 </div>
                 <div class="col-md-6" data-aos="fade-left">
                     <img src="{{ asset('front/img/undraw_svg_2.svg') }}" alt="Image" class="img-fluid">
@@ -30,8 +61,7 @@
             <div class="row align-items-center">
                 <div class="col-md-4 ml-auto order-2">
                     <h2 class="mb-4">Gather Feedback</h2>
-                    <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur at reprehenderit optio, laudantium eius quod, eum maxime molestiae porro omnis. Dolores aspernatur delectus impedit incidunt dolore mollitia esse natus beatae.</p>
-                    <p><a href="#">Read More</a></p>
+                    <p class="mb-4">We work as a team with our clients on the issue of picking the best appropriate plan as indicated by their requirements. </p>
                 </div>
                 <div class="col-md-6" data-aos="fade-right">
                     <img src="{{ asset('front/img/undraw_svg_3.svg') }}" alt="Image" class="img-fluid">
@@ -44,7 +74,7 @@
             <div class="row align-items-center">
                 <div class="col-md-4 mr-auto">
                     <h2 class="mb-4">Present Designs Inspiration</h2>
-                    <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur at reprehenderit optio, laudantium eius quod, eum maxime molestiae porro omnis. Dolores aspernatur delectus impedit incidunt dolore mollitia esse natus beatae.</p>
+                    <p class="mb-4">CypherOcean innovations offer all graphic planning types of assistance. We take an idea and transform it into an amazing and successful piece of correspondence. </p>
                     <p><a href="#">Read More</a></p>
                 </div>
                 <div class="col-md-6" data-aos="fade-left">
@@ -58,7 +88,7 @@
             <div class="row align-items-center">
                 <div class="col-md-4 ml-auto order-2">
                     <h2 class="mb-4">Powerful App Design </h2>
-                    <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur at reprehenderit optio, laudantium eius quod, eum maxime molestiae porro omnis. Dolores aspernatur delectus impedit incidunt dolore mollitia esse natus beatae.</p>
+                    <p class="mb-4">CypherOcean Technologies has practical experience in administrations like Animations, Template planning, Logo planning, Brochure planning, corporate card planning, Branding, and bundle planning. We have encountered creators and engineers in planning projects with creative layouts.</p>
                     <p><a href="#">Read More</a></p>
                 </div>
                 <div class="col-md-6" data-aos="fade-right">
