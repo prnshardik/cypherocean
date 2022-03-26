04d4c983988d98ca657c5d5153e9b799152271b9
Gajjar-Mitul <gajjarmitul007@gmail.com>
2022-03-26 16:34:27 +0530

    last

--- resources/views/front/layout/footer.blade.php
+++ resources/views/front/layout/footer.blade.php
@@ -22,15 +22,15 @@
                         <label for="rating">Star:</label>
                         <div id="full_stars_example_two">
                             <div class="rating-group">
-                                <label aria-label="1 star" class="rating__label" for="rating-1"><i class="rating__icon rating__icon--star fa fa-star"></i></label>
+                                <label aria-label="1 star" class="rating__label" for="rating-1"><i class="rating__icon rating__icon--star fa fa-star fa-2x"></i></label>
                                 <input class="rating__input" name="rating" id="rating-1" value="1" type="radio">
-                                <label aria-label="2 stars" class="rating__label" for="rating-2"><i class="rating__icon rating__icon--star fa fa-star"></i></label>
+                                <label aria-label="2 stars" class="rating__label" for="rating-2"><i class="rating__icon rating__icon--star fa fa-star fa-2x"></i></label>
                                 <input class="rating__input" name="rating" id="rating-2" value="2" type="radio">
-                                <label aria-label="3 stars" class="rating__label" for="rating-3"><i class="rating__icon rating__icon--star fa fa-star"></i></label>
+                                <label aria-label="3 stars" class="rating__label" for="rating-3"><i class="rating__icon rating__icon--star fa fa-star fa-2x"></i></label>
                                 <input class="rating__input" name="rating" id="rating-3" value="3" type="radio">
-                                <label aria-label="4 stars" class="rating__label" for="rating-4"><i class="rating__icon rating__icon--star fa fa-star"></i></label>
+                                <label aria-label="4 stars" class="rating__label" for="rating-4"><i class="rating__icon rating__icon--star fa fa-star fa-2x"></i></label>
                                 <input class="rating__input" name="rating" id="rating-4" value="4" type="radio">
-                                <label aria-label="5 stars" class="rating__label" for="rating-5"><i class="rating__icon rating__icon--star fa fa-star"></i></label>
+                                <label aria-label="5 stars" class="rating__label" for="rating-5"><i class="rating__icon rating__icon--star fa fa-star fa-2x"></i></label>
                                 <input class="rating__input" name="rating" id="rating-5" value="5" type="radio" checked="checked">
                             </div>
                             <span class="kt-form__help error rating"></span>
@@ -41,8 +41,8 @@
                         <textarea class="form-control" name="feedback" placeholder="Enter your Feedback"></textarea>
                         <span class="kt-form__help error feedback"></span>
                     </div>
-                    <div class="form-group col-sm-12 text-right">
-                        <input type="submit" class="btn btn-primary d-block w-10" value="Submit">
+                    <div class="form-group col-sm-12 d-flex justify-content-end align-item-end w-10">
+                        <input type="submit" class="btn btn-primary" value="Submit">
                     </div>
                 </div>
             </form>
